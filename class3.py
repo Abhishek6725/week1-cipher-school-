478084ad832da24f32c811301fb3a98c30baa766
Abhishek6725 <118000282+Abhishek6725@users.noreply.github.com>
2022-12-01 22:22:45 +0530

    Add files via upload

---
+++ class3.py
@@ -0,0 +1,16 @@
+#knowing more about print
+
+print("Abhishek Kumar")
+print(1)
+
+
+#print can accept multiple values at once
+print(1, 2, 3, "Abhishek", 3.4, 1 + 3j)
+
+
+print("hello", end=";")
+print("world")
+
+
+
+print(1, 2, 3, "Abhishek", 3.4, 1 + 3j, sep=",")
